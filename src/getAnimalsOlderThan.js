23e207d5542d929fceb59aa759a87c77830c5228
Anna Escolaro <annaescolaro@hotmail.com>
2023-05-12 18:58:35 -0300

    req 1 e 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const animalType = species.filter((specie) => specie.name === animal);
+  const [animalTypeObj] = animalType;
+  const verifyAge = animalTypeObj.residents.every((resident) => resident.age >= age);
+  return verifyAge;
 };
+
+console.log(getAnimalsOlderThan('lions', 12));
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,23 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...args) => {
+  const filteredSpecies = [];
+  const allSpecies = species;
+  //   for (let index = 0; index < args.length; index += 1) {
+  //     for (let index = 0; index < allSpecies.length; index += 1) {
+  //       if (args[index] === allSpecies[index].id) {
+  //         filteredSpecies.push(allSpecies[index]);
+  //       }
+  //     }
+  //   }
+  args.forEach((item) => allSpecies.forEach((specie) => {
+    if (item === specie.id) {
+      filteredSpecies.push(specie);
+    }
+  }));
+  return filteredSpecies;
 };
+
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 module.exports = getSpeciesByIds;
