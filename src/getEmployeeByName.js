fec2a3047f0832913143f2eaac399587243f9270
Anna Escolaro <annaescolaro@hotmail.com>
2023-05-12 19:24:21 -0300

    corrige lint req 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,11 +3,10 @@ const { employees } = require('../data/zoo_data');
 const getEmployeeByName = (employeeName) => {
   if (!employeeName) {
     return {};
-  };
+  }
   const selectedEmployee = employees.find((employee) =>
     employee.firstName === employeeName || employee.lastName === employeeName);
   return selectedEmployee;
 };
-console.log(getEmployeeByName());
-console.log(getEmployeeByName('Nigel'));
+
 module.exports = getEmployeeByName;
