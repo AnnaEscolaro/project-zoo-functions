a7357e4477190da162348eb70c1acfa16962c37b
Anna Escolaro <annaescolaro@hotmail.com>
2023-05-15 20:20:34 -0300

    req 5]

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const countAnimals = (animals) => {
+  if (!animals) {
+    const allAnimals = species.reduce((acc, element) => {
+      acc[element.name] = element.residents.length;
+      return acc;
+    }, {});
+    return allAnimals;
+  }
+
+  if (animals.species && animals.sex) {
+    const selectedAnimal = species.find((specie) => specie.name === animals.species);
+    return selectedAnimal.residents.filter((resident) => resident.sex === animals.sex).length;
+  }
+
+  if (animals.species) {
+    return species.find((specie) =>
+      specie.name === animals.species).residents.length;
+  }
 };
+
+console.log(countAnimals());
+console.log(countAnimals({ species: 'penguins' }));
+console.log(countAnimals({ species: 'giraffes', sex: 'female' }));
 
 module.exports = countAnimals;
